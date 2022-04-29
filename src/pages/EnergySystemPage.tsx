7de18b0de48b95b1619fb40f76a5c9659a454c0c
Taesoo Lim <limtsoo.shina@gmail.com>
2022-04-29 11:30:55 +0900

    Apply ridge state scenario on pie chart

--- src/pages/EnergySystemPage.tsx
+++ src/pages/EnergySystemPage.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { BarChart } from '../components/chartjs/BarChart';
 import { PieChart } from '../components/chartjs/PieChart';
 import { FillterBar } from '../components/FillterBar';
-
-const Labels2 = ['WT', 'PV', 'CHP', 'Fcell'];
-const Prices2 = [2, 5, 2, 8];
+import { simulationState } from '../plugins/ridge';
 
 export const EnergySystemPage = () => {
   return (
@@ -13,9 +11,8 @@ export const EnergySystemPage = () => {
         <div className="grid grid-cols-2 gap-10">
           {/* <div className="h-[100px]"> */}
           <PieChart
-            labels={Labels2}
-            amount={Prices2}
             title="발전설비별 전력생산비중"
+            simulation={ simulationState.useSelector((state) => state?.facility_configuration) }
           />
           {/* </div> */}
           <BarChart title="발전설비구성" />
