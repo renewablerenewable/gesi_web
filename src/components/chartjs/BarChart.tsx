535dd67d442d77d9a5850d2ddca2d59f67cff2de
Taesoo Lim <tslim@mnc.ai>
2022-04-29 09:42:22 +0900

    Apply ridge state scenario on bar chart

--- src/components/chartjs/BarChart.tsx
+++ src/components/chartjs/BarChart.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Chart as ChartJS,
+  ChartData,
   CategoryScale,
   LinearScale,
   BarElement,
@@ -13,9 +14,46 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
 interface BarChartProps {
   title?: string;
+  simulation?: object;
 }
 
-export const BarChart: React.FC<BarChartProps> = ({ title }) => {
+export const BarChart: React.FC<BarChartProps> = ({ title, simulation }) => {
+  const [data, setData] = useState<ChartData<"bar", number[], unknown>>({
+  labels: [''],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [5],
+      backgroundColor: '#ED6E85',
+      barThickness: 50,
+    },
+    {
+      label: 'Dataset 2',
+      data: [3],
+      backgroundColor: '#F1A354',
+      barThickness: 50,
+    },
+    {
+      label: 'Dataset 3',
+      data: [12],
+      backgroundColor: '#F7CE6B',
+      barThickness: 50,
+    },
+    {
+      label: 'Dataset 3',
+      data: [20],
+      backgroundColor: '#4598F8',
+      barThickness: 50,
+    },
+    {
+      label: 'Dataset 3',
+      data: [15],
+      backgroundColor: '#7845F6',
+      barThickness: 50,
+    },
+  ],
+
+  });
   const options = {
     responsive: true,
     plugins: {
@@ -29,41 +67,23 @@ export const BarChart: React.FC<BarChartProps> = ({ title }) => {
     },
   };
 
-  const data = {
-    labels: [''],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [5],
-        backgroundColor: '#ED6E85',
+  if (simulation) {
+    let newData: ChartData<"bar", number[], unknown> = JSON.parse(JSON.stringify(data));
+    newData.datasets.length = 0;
+    Object.entries(simulation).forEach(([key, value]) => {
+      newData.datasets.push({
+        label: key,
+        data: [value],
         barThickness: 50,
-      },
-      {
-        label: 'Dataset 2',
-        data: [3],
-        backgroundColor: '#F1A354',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [12],
-        backgroundColor: '#F7CE6B',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [20],
-        backgroundColor: '#4598F8',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [15],
-        backgroundColor: '#7845F6',
-        barThickness: 50,
-      },
-    ],
-  };
+      });
+    });
+    console.log(newData.datasets, data.datasets)
+    if (JSON.stringify(newData.datasets) !== JSON.stringify(data.datasets)) {
+      // console.log(newData.datasets, data.datasets)
+      setData(newData);
+      // console.log("new Data: ", newData);
+    }
+  }
 
   return (
     <div className="w-full">
--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BarChart } from '../components/chartjs/BarChart';
 import { StackedMultiBarChart } from '../components/chartjs/StackedMultiBarChart';
 import { FillterBar } from '../components/FillterBar';
+import { simulationState } from '../plugins/ridge';
 
 export const OverviewPage = () => {
   return (
@@ -9,7 +10,7 @@ export const OverviewPage = () => {
       <div className="border p-5 bg-white my-5 mx-4">
         <div className="grid grid-cols-2 gap-10">
           <StackedMultiBarChart title="최종에너지 소비" />
-          <BarChart title="온실가스 배출량" />
+          <BarChart title="온실가스 배출량" simulation={ simulationState.useSelector((state) => state?.emissions) } />
         </div>
       </div>
       <FillterBar />
