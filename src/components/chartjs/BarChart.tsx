43d419fdfd8290c0b2ed394badb7e67239a636af
Taesoo Lim <tslim@mnc.ai>
2022-04-29 10:45:42 +0900

    Apply background color and delete console logs

--- src/components/chartjs/BarChart.tsx
+++ src/components/chartjs/BarChart.tsx
@@ -10,50 +10,58 @@ import {
 } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
+ChartJS.register(
+  CategoryScale, 
+  LinearScale, 
+  BarElement, 
+  Title, 
+  Tooltip
+);
 
 interface BarChartProps {
   title?: string;
   simulation?: object;
 }
 
+const backgroundColor: string[] = ['#ED6E85', '#F1A354', '#F7CE6B', '#4598F8', '#7845F6']
+
 export const BarChart: React.FC<BarChartProps> = ({ title, simulation }) => {
   const [data, setData] = useState<ChartData<"bar", number[], unknown>>({
-  labels: [''],
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [5],
-      backgroundColor: '#ED6E85',
-      barThickness: 50,
-    },
-    {
-      label: 'Dataset 2',
-      data: [3],
-      backgroundColor: '#F1A354',
-      barThickness: 50,
-    },
-    {
-      label: 'Dataset 3',
-      data: [12],
-      backgroundColor: '#F7CE6B',
-      barThickness: 50,
-    },
-    {
-      label: 'Dataset 3',
-      data: [20],
-      backgroundColor: '#4598F8',
-      barThickness: 50,
-    },
-    {
-      label: 'Dataset 3',
-      data: [15],
-      backgroundColor: '#7845F6',
-      barThickness: 50,
-    },
-  ],
-
+    labels: [''],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [5],
+        backgroundColor: '#ED6E85',
+        barThickness: 50,
+      },
+      {
+        label: 'Dataset 2',
+        data: [3],
+        backgroundColor: '#F1A354',
+        barThickness: 50,
+      },
+      {
+        label: 'Dataset 3',
+        data: [12],
+        backgroundColor: '#F7CE6B',
+        barThickness: 50,
+      },
+      {
+        label: 'Dataset 3',
+        data: [20],
+        backgroundColor: '#4598F8',
+        barThickness: 50,
+      },
+      {
+        label: 'Dataset 3',
+        data: [15],
+        backgroundColor: '#7845F6',
+        barThickness: 50,
+      },
+    ],
   });
+
   const options = {
     responsive: true,
     plugins: {
@@ -68,20 +76,20 @@ export const BarChart: React.FC<BarChartProps> = ({ title, simulation }) => {
   };
 
   if (simulation) {
+    // Deep copy data and clear datasets
     let newData: ChartData<"bar", number[], unknown> = JSON.parse(JSON.stringify(data));
     newData.datasets.length = 0;
-    Object.entries(simulation).forEach(([key, value]) => {
+
+    Object.entries(simulation).forEach(([key, value], index) => {
       newData.datasets.push({
         label: key,
         data: [value],
         barThickness: 50,
+        backgroundColor: backgroundColor[index]
       });
     });
-    console.log(newData.datasets, data.datasets)
     if (JSON.stringify(newData.datasets) !== JSON.stringify(data.datasets)) {
-      // console.log(newData.datasets, data.datasets)
       setData(newData);
-      // console.log("new Data: ", newData);
     }
   }
 
