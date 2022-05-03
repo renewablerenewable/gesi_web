e3b78d67f92768c72491201dc211107b8ad03940
Taesoo Lim <limtsoo.shina@gmail.com>
2022-05-03 11:25:57 +0900

    Update footer

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -4,9 +4,9 @@ export const BottomBar = () => {
   return (
     <div className="w-screen bg-white py-10 text-sm text-gray-800">
       <div className="mx-auto max-w-screen-2xl px-4">
-        대표자 : 최용철,강정모 ㅣ 상호명 : 엔트로피패러독스 ㅣ 사업자등록번호 :
-        264-88-01993 ㅣ 주소 : 서울시 강남구 도산대로100길 24, 3층 <br />© 2020
-        Entropy Paradox, inc. All Rights Reserved.
+      사단법인 녹색에너지전략연구소 | 사업자등록번호 220-82-11150‌ | Tel. 02-552-0940 | E-mail. gesi@gesi.kr |<br/>
+      주소 05836 서울특별시 송파구 법원로 11길 25, H비지니스파크 B동 515‌호 <br/><br/>
+      COPYRIGHT ‌GESI 2020. ALL RIGHTS RESERVED.
       </div>
     </div>
   );
