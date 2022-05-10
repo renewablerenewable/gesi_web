93fa665876bf83b852c1ff37d9cbc10b75e01b5a
Taesoo Lim <limtsoo.shina@gmail.com>
2022-05-10 21:29:57 +0900

    Apply ridge state scenario on StackedBarChart and finish P2X page

--- src/components/chartjs/StackedBarChart.tsx
+++ src/components/chartjs/StackedBarChart.tsx
@@ -22,43 +22,60 @@ ChartJS.register(
 
 interface StackedBarChartProps {
   title?: string;
+  labels?: string[];
   simulation?: object;
+  labelMap?: { [name: string]: string };
 }
 
-export const StackedBarChart: React.FC<StackedBarChartProps> = ({ title, simulation }) => {
+const backgroundColor: string[] = [
+  '#ED6E85',
+  '#F1A354',
+  '#F7CE6B',
+  '#6CBDBF',
+  '#4598F8',
+  '#7845F6',
+  '#C9CBCF',
+]
+
+export const StackedBarChart: React.FC<StackedBarChartProps> = ({ 
+  title, 
+  labels,
+  simulation,
+  labelMap,
+}) => {
   const [data, setData] = useState<ChartData<"bar", number[], unknown>>({
     labels: [''],
     datasets: [
-      {
-        label: 'Dataset 1',
-        data: [5],
-        backgroundColor: '#ED6E85',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 2',
-        data: [3],
-        backgroundColor: '#F1A354',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [10],
-        backgroundColor: '#F7CE6B',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [10],
-        backgroundColor: '#4598F8',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [10],
-        backgroundColor: '#7845F6',
-        barThickness: 50,
-      },
+      // {
+      //   label: 'Dataset 1',
+      //   data: [5],
+      //   backgroundColor: '#ED6E85',
+      //   barThickness: 50,
+      // },
+      // {
+      //   label: 'Dataset 2',
+      //   data: [3],
+      //   backgroundColor: '#F1A354',
+      //   barThickness: 50,
+      // },
+      // {
+      //   label: 'Dataset 3',
+      //   data: [10],
+      //   backgroundColor: '#F7CE6B',
+      //   barThickness: 50,
+      // },
+      // {
+      //   label: 'Dataset 3',
+      //   data: [10],
+      //   backgroundColor: '#4598F8',
+      //   barThickness: 50,
+      // },
+      // {
+      //   label: 'Dataset 3',
+      //   data: [10],
+      //   backgroundColor: '#7845F6',
+      //   barThickness: 50,
+      // },
     ],
   });
 
@@ -88,7 +105,36 @@ export const StackedBarChart: React.FC<StackedBarChartProps> = ({ title, simulat
     },
   };
 
+  if (simulation) {
+    let newData: ChartData<"bar", number[], unknown> = JSON.parse(JSON.stringify(data));
+    newData.datasets.length = 0;
 
+    if (labels) {
+      for (const [idx, label] of labels.entries()) {
+        if (simulation.hasOwnProperty(label)) {
+          type _keyType = keyof typeof simulation;
+          const _key = label as _keyType;
+
+          let newLabel = label;
+
+          if (labelMap)
+            newLabel = labelMap[label as string];
+
+          newData.datasets.push({
+            label: newLabel,
+            data: [simulation[_key]],
+            barThickness: 50,
+            backgroundColor: backgroundColor[idx]
+          });
+        }
+      }
+    }
+
+    if (JSON.stringify(newData.datasets) !== JSON.stringify(data.datasets)) {
+      setData(newData);
+    }
+  }
+  
 
   return (
     <div className="w-full">
--- src/pages/P2XPage.tsx
+++ src/pages/P2XPage.tsx
@@ -2,14 +2,38 @@ import React from 'react';
 import { BarChart } from '../components/chartjs/BarChart';
 import { StackedBarChart } from '../components/chartjs/StackedBarChart';
 import { FillterBar } from '../components/FillterBar';
+import { simulationState } from '../plugins/ridge';
+
+const P2XFacilityConfigurationLabels = ['DH_HP', 'DH_Boiler', 'Electric_boiler', 'Electrolysis', 'Gas_interface', 'b_interface', 'pumped', 'TES_DH', 'Gas_storage', 'Battery']
+const P2XFacilityConfigurationChartOptions = {
+  barThickness: 30
+}
+
+const P2XEnergyBalancingLabels = ['Electricity_demand', 'Other', 'Nuke', 'coal_PP', 'NG_PP_existing', 'NG_PP_new', 'Fcell', 'CHP', 'PV', 'WT']
+
+const P2XExcessProductionUsageLabels = ['Curtailment', 'P2H', 'EV', 'Electric_boiler', 'Electrolysis']
 
 export const P2XPage = () => {
   return (
     <div>
       <div className="border p-5 bg-white my-5 mx-4">
-        <div className="grid grid-cols-2 gap-10">
-          <BarChart title="P2X 설비 구성" />
-          <StackedBarChart title="에너지 밸런싱" />
+        <div className="grid grid-cols-3 gap-10">
+          <BarChart 
+            title="P2X 설비 구성"
+            labels={ P2XFacilityConfigurationLabels }
+            simulation={ simulationState.useSelector((state) => state?.facility_configuration) } 
+            dataOptions={ P2XFacilityConfigurationChartOptions }
+          />
+          <StackedBarChart 
+            title="에너지 밸런싱" 
+            labels={ P2XEnergyBalancingLabels }
+            simulation={ simulationState.useSelector((state) => state?.power_generation) }
+          />
+          <StackedBarChart 
+            title="초과 생산량 활용" 
+            labels={ P2XExcessProductionUsageLabels }
+            simulation={ simulationState.useSelector((state) => state?.power_generation) }
+          />
         </div>
       </div>
       <FillterBar />
