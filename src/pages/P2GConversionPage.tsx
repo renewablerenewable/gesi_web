32b3ac56bb5c212239d5fc785ec35a6f95c098aa
Taesoo Lim <limtsoo.shina@gmail.com>
2022-05-17 09:02:47 +0900

    Sync data of P2GConvertionPage

--- src/pages/P2GConversionPage.tsx
+++ src/pages/P2GConversionPage.tsx
@@ -6,22 +6,77 @@ import { simulationState } from '../plugins/ridge';
 
 // const Labels2 = ['발전', '수송', '산업원료'];
 // const Prices2 = [5, 2, 8];
+const hydrogenDemandLabels = ['electricity_generation', 'transportation', 'industrial_material'];
+const hydrogenDemandLabelMap = {
+  'electricity_generation': '발전',
+  'transportation': '수송',
+  'industrial_material': '산업원료',
+}
+const upperLineChartLabels = ['gas_demand', 'gas_production', 'gas_discharging']
+const upperLineChartOptions = {
+  stacked: true,
+  xlabels: true,
+  legend: 'top',
+}
+const lowerLineChartLabels = ['gas_charging', 'gas_discharging', 'gas_SOC']
+const lowerLineChartOptions = {
+  stacked: true,
+  xlabels: false,
+  legend: 'bottom',
+  negative: true,
+}
+const summerRange = {
+  start: 1,
+  end: 71
+}
+const winterRange = {
+  start: 5378,
+  end: 5450
+}
 
 export const P2GConversionPage = () => {
   return (
     <div>
       <div className="border p-5 bg-white my-5 mx-4">
-        {/* <div className="grid grid-cols-3 gap-10">
-          <PieChart
-            // labels={Labels2}
-            // amount={Prices2}
-            title="발전설비별 전력생산비중"
-            simulation={ simulationState.useSelector((state) => state?.P2G_hydrogen_demand) }
+        <div className="grid grid-rows-2 grid-cols-3 gap-10">
+          <div className='row-span-2'>
+            <PieChart
+              // labels={Labels2}
+              // amount={Prices2}
+              title="발전설비별 전력생산비중"
+              labels={ hydrogenDemandLabels }
+              simulation={ simulationState.useSelector((state) => state?.P2G_hydrogen_demand) }
+              labelMap={ hydrogenDemandLabelMap }
+            />
+          </div>
+          <LineChart 
+            title="여름"
+            labels={ upperLineChartLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep_g) }
+            dataOptions={ upperLineChartOptions }
+            range={ summerRange }
           />
-          <LineChart title="여름" />
-          <LineChart title="겨울" />
-        </div> */}
-        <div className="grid grid-cols-2 gap-10">
+          <LineChart 
+            title="겨울"
+            labels={ upperLineChartLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep_g) }
+            dataOptions={ upperLineChartOptions }
+            range={ winterRange }
+          />
+          <LineChart
+            labels={ lowerLineChartLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep_g) }
+            dataOptions={ lowerLineChartOptions }
+            range={ summerRange }
+          />
+          <LineChart
+            labels={ lowerLineChartLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep_g) }
+            dataOptions={ lowerLineChartOptions }
+            range={ winterRange }
+            />
+        </div>
+        {/* <div className="grid grid-cols-2 gap-10">
           <PieChart
             // labels={Labels2}
             // amount={Prices2}
@@ -33,7 +88,7 @@ export const P2GConversionPage = () => {
             title="주요 지표"
             simulation={ simulationState.useSelector((state) => state?.P2G_core_indicators) }
           />
-        </div>
+        </div> */}
       </div>
       <FillterBar />
     </div>
