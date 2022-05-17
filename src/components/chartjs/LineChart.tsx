0ee2d019d49032346dc07fba23378d6236904084
Taesoo Lim <limtsoo.shina@gmail.com>
2022-05-17 09:00:32 +0900

    Apply simulation ridge state on lineChart and Finish P2XBalancingPage

--- src/components/chartjs/LineChart.tsx
+++ src/components/chartjs/LineChart.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Chart as ChartJS,
+  ChartData,
   CategoryScale,
+  Filler,
   LinearScale,
-  PointElement,
   LineElement,
+  PointElement,
   Title,
   Tooltip,
   Legend,
@@ -13,9 +15,10 @@ import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
+  Filler,
   LinearScale,
-  PointElement,
   LineElement,
+  PointElement,
   Title,
   Tooltip,
   Legend
@@ -23,14 +26,48 @@ ChartJS.register(
 
 interface LineChartProps {
   title?: string;
+  labels?: string[];
+  simulation?: object;
+  dataOptions?: object;
+  range?: {[name: string]: number};
 }
 
-export const LineChart: React.FC<LineChartProps> = ({ title }) => {
-  const options = {
+const backgroundColor: string[] = [
+  '#8dd3c7', 
+  '#ffffb3', 
+  '#bebada', 
+  '#fb8072', 
+  '#80b1d3', 
+  '#fdb462', 
+  '#b3de69', 
+  '#fccde5', 
+  '#d9d9d9', 
+  '#bc80bd', 
+  '#ccebc5', 
+  '#ffed6f'
+];
+
+export const LineChart: React.FC<LineChartProps> = ({ 
+  title, 
+  labels,
+  simulation,
+  dataOptions,
+  range
+ }) => {
+  const [data, setData] = useState<ChartData<"line", number[], unknown>>({
+    labels: [''],
+    datasets: [
+      {
+        data: [],
+      }
+    ]
+  });
+
+  let options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: undefined
       },
       title: {
         display: true,
@@ -42,34 +79,108 @@ export const LineChart: React.FC<LineChartProps> = ({ title }) => {
         }
       },
     },
+    elements: {
+      point: {
+        radius: 0,
+      }
+    },
+    interaction: {
+      // mode: 'nearest',
+      // axis: 'x',
+      intersect: false
+    },
+    scales: {
+      y: {
+        stacked: undefined,
+      }
+    }
   };
 
-  const data = {
-    labels: ['', '', '', '', '', ''],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [5, 1, 4, 6, 7, 10],
-        borderColor: '#ED6E85',
-        backgroundColor: '#ED6E85',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 2',
-        data: [5, 6, 12, 46, 1, 18],
-        borderColor: '#F1A354',
-        backgroundColor: '#F1A354',
-        barThickness: 50,
-      },
-      {
-        label: 'Dataset 3',
-        data: [12, 16, 2, 18, 30, 11],
-        borderColor: '#F7CE6B',
-        backgroundColor: '#F7CE6B',
-        barThickness: 50,
-      },
-    ],
-  };
+  if (simulation) {
+    let newData: ChartData<"line", number[], unknown> = JSON.parse(JSON.stringify(data));
+    newData.datasets.length = 0;
+
+    let newLabels: string[] = [];
+  
+    let start = 0;
+    let end = 1;
+    let negative = false;
+
+    if (range) {
+      start = range.start;
+      end = range.end;
+    }
+
+    newLabels = Array(end - start).fill('');
+
+    if (dataOptions) {
+      Object.entries(dataOptions).forEach(([key, value], index) => {
+        if (key === 'legend')
+          options.plugins.legend.position = value;
+        if (key === 'stacked')
+          options.scales.y.stacked = value;
+        
+        if (key === 'xlabels' && value === true) {
+          newLabels = [];
+          for (let i: number = 1; i <= (end - start + 1); i++) {
+            newLabels.push(i.toString());
+          }
+        }
+
+        if (key === 'negative')
+          negative = value;
+
+      });
+    }
+    newData.labels = newLabels;
+
+    if (labels) {
+      for (const [index, label] of labels.entries()) {
+        if (simulation.hasOwnProperty(label)) {
+          type _keyType = keyof typeof simulation;
+          const _key = label as _keyType;
+          const value = simulation[_key];
+          let newDatasetData: number[] = [];
+
+          for (let i:number = start; i <= end; i++) {
+            if (negative)
+              newDatasetData.push(value[i.toString()] * (-1));
+            else
+              newDatasetData.push(value[i.toString()]);
+          }
+          
+          newData.datasets.push({
+            label: label,
+            data: newDatasetData,
+            borderColor: backgroundColor[index],
+            backgroundColor: backgroundColor[index],
+            fill: true
+          });
+        }
+      }
+    } else {
+      Object.entries(simulation).forEach(([key, value], index) => {
+        if (key !== 'total') {
+          let newDatasetData: number[] = [];
+  
+          for (let i:number = start; i <= end; i++) 
+            newDatasetData.push(value[i.toString()]);
+          
+          newData.datasets.push({
+            label: key,
+            data: newDatasetData,
+            borderColor: backgroundColor[index],
+            backgroundColor: backgroundColor[index],
+            fill: true
+          })
+        }
+      });
+    }
+
+    if (JSON.stringify(newData.datasets) !== JSON.stringify(data.datasets)) {
+      setData(newData);
+    }
+  }
 
   return (
     <div className="w-full">
--- src/pages/P2XBalancingPage.tsx
+++ src/pages/P2XBalancingPage.tsx
@@ -1,37 +1,64 @@
 import React from 'react';
-// import { LineChart } from '../components/chartjs/LineChart';
-import { StackedMultiBarChart } from '../components/chartjs/StackedMultiBarChart';
+import { LineChart } from '../components/chartjs/LineChart';
 import { FillterBar } from '../components/FillterBar';
-import { scenarioState, simulationState } from '../plugins/ridge'
+import { simulationState } from '../plugins/ridge'
+
+const upperLabels = ['el_demand', 'Nuke', 'coal_PP', 'NG_PP', 'PP', 'CHP', 'PV', 'WT', 'other']
+const upperChartOptions = {
+  stacked: true,
+  xlabels: true,
+  legend: 'top',
+}
+const lowerLabels = ['EV', 'P2H', 'E_boiler', 'battery_in', 'pumped_in', 'electrolysis', 'curtail', 'battery_out', 'pumped_out']
+const lowerChartOption = {
+  stacked: true,
+  xlabels: false,
+  legend: 'bottom',
+  negative: true,
+}
+
+const summerRange = {
+  start: 1,
+  end: 71
+}
+
+const winterRange = {
+  start: 5378,
+  end: 5450
+}
 
 export const P2XBalancingPage = () => {
-  let targetYear: string = "2030"
-  if (scenarioState.useSelector((state) => state.target))
-    targetYear = "2050"
-  
-  const order: string[] = ['init_year', 'end_year']
-  
   return (
     <div>
       <div className="border p-5 bg-white my-5 mx-4">
-        {/* <div className="grid grid-cols-2 gap-10">
-          <LineChart title="여름" />
-          <LineChart title="겨울" />
-        </div> */}
         <div className="grid grid-cols-2 gap-10">
-          <StackedMultiBarChart 
-            title='가정 내 난방용 에너지 소비 변화'
-            simulation={ simulationState.useSelector((state) => state?.P2H_consumption_change.home) }
-            labels={ ["2020", targetYear ] }
-            order={ order }
-            reverse={ true }
+          <LineChart 
+            title="여름" 
+            labels={ upperLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep) } 
+            dataOptions={ upperChartOptions } 
+            range={ summerRange }
           />
-          <StackedMultiBarChart 
-            title='산업 내 난방용 에너지 소비 변화'
-            simulation={ simulationState.useSelector((state) => state?.P2H_consumption_change.commerce) }
-            labels={ ["2020", targetYear ] }
-            order={ order }
-            reverse={ true }
+          <LineChart 
+            title="겨울" 
+            labels={ upperLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep) } 
+            dataOptions={ upperChartOptions } 
+            range={ winterRange }
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-10">
+          <LineChart 
+            labels={ lowerLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep) } 
+            dataOptions={ lowerChartOption } 
+            range={ winterRange }
+          />
+          <LineChart 
+            labels={ lowerLabels }
+            simulation={ simulationState.useSelector((state) => state?.rep) } 
+            dataOptions={ lowerChartOption } 
+            range={ summerRange }
           />
         </div>
       </div>
