3edb756fcda8d7ae105f8172b3aa36eb0bddff40
HYODONGMOON <89068126+HYODONGMOON@users.noreply.github.com>
2022-06-09 11:43:04 +0900

    Update EnergySystemPage.tsx

--- src/pages/EnergySystemPage.tsx
+++ src/pages/EnergySystemPage.tsx
@@ -6,9 +6,31 @@ import { simulationState } from '../plugins/ridge';
 
 // Power Generation Chart Configuration
 const powerGenerationLabels = ['WT', 'PV', 'CHP', 'Fcell', 'NG_PP_new', 'NG_PP_existing', 'coal_PP', 'Nuke', 'Other']
+const powerGenerationLabelMap = {
+  'WT' : '풍력',
+  'PV' : '태양광',
+  'CHP' : '열병합',
+  'Fcell' : '연료전지',
+  'NG_PP_new' : 'LNG화력(신규)',
+  'NG_PP_existing' : 'LNG화력(기존)',
+  'coal_PP' : '석탄화력', 
+  'Nuke' : '원자력', 
+  'Other' : '기타',
+}
 
 // Facility Configuration Chart Configuration
 const facilityConfigurationLabels = ['Wind_on', 'Wind_off', 'PV', 'CHP', 'Fcell', 'NG_PP_new', 'NG_PP_existing', 'coal_PP', 'Nuke']
+const facilityConfigurationLabelMap = {
+  'Wind_on' : '육상풍력',
+  'Wind_off' : '해상풍력',
+  'PV' : '태양광',
+  'CHP' : '열병합',
+  'Fcell' : '연료전지',
+  'NG_PP_new' : 'LNG화력(신규)',
+  'NG_PP_existing' : 'LNG화력(기존)',
+  'coal_PP' : '석탄화력', 
+  'Nuke' : '원자력', 
+}  
 const facilityConfigurationChartOptions = {
   // barThickness: 20
   max: 500000
